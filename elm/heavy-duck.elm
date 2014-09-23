92b8c2d9955500e4227e23698b992d15ddb586a5
David Boissier <david.boissier@gmail.com>
2014-09-23 15:20:42 +0200

    display a duck in a game screen

---
+++ elm/heavy-duck.elm
@@ -0,0 +1,23 @@
+import Keyboard
+
+-- Inputs
+delta = inSeconds <~ fps 30
+
+
+-- Model
+rabbit = image 80 100 "http://www.canardpc.com/img/couly/img141.png"
+
+-- Updates
+
+
+
+-- Display
+bgBlue = rgb 100 220 255
+rectWidth  = 800
+recHeight = 600
+
+main : Element
+main = collage rectWidth recHeight [
+             filled bgBlue <| rect rectWidth recHeight
+           , toForm rabbit
+       ]
